fc1c74f8efbc56d677aaf55fbbe5928e4d5ea137
dlope26 <dlope26@bancodebogota.com.co>
2025-07-04 17:36:22 -0500

    new run12

--- src/test/java/starter/tasks/login/Login.java
+++ src/test/java/starter/tasks/login/Login.java
@@ -4,7 +4,10 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 import starter.ui.login.LoginPage;
+
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class Login implements Task {
       private final String username;
@@ -17,6 +20,7 @@ public class Login implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
+                WaitUntil.the(LoginPage.INPUT_USERNAME, isVisible()).forNoMoreThan(10).seconds(),
                 Enter.theValue("user").into(LoginPage.INPUT_USERNAME),
                 Enter.theValue("pass").into(LoginPage.INPUT_PASSWORD),
                 Click.on(LoginPage.BUTTON_LOG_IN)
